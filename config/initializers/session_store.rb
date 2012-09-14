1855583cfcac8235ac75aef3fe7cbd4fd970a037
twonegatives <twonegatives@ubuntu.ubuntu-domain>
2012-09-14 21:34:25 +0800

    fix for some mistakes, TODO notes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Organizer::Application.config.session_store :cookie_store, :key => '_organizer_session'
+# TODO: сделать общие куки для всех поддоменов
+Organizer::Application.config.session_store :cookie_store, key: '_organizer_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
