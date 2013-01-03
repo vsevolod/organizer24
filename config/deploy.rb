021dc87be706b89f05302ce6ac7e169a5fd03c0a
Vsevolod <gsevka@gmail.com>
2013-01-03 14:17:01 +0800

    complete bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,12 +21,13 @@ role :web, "organizer24.ru"
 role :app, "organizer24.ru"
 role :db,  "organizer24.ru"
 
-after "deploy:update_code", :copy_database_config
+after "deploy:update_code", :copy_config_files
 after "deploy:update_code", :add_log_files
 after "deploy:update_code", :precompile
 
-task :copy_database_config, roles => :app do
+task :copy_config_files, roles => :app do
   run "cp #{shared_path}/database.yml #{release_path}/config/database.yml"
+  run "cp #{shared_path}/smsru.yml #{release_path}/config/smsru.yml"
 end
 
 task :add_log_files, roles => :app do
@@ -54,17 +55,17 @@ task :precompile do
 end
 
 set :rails_env, :production
-set :unicorn_binary, "/home/vsevolod/.rvm/gems/ruby-1.9.3-p327/gems/unicorn-4.5.0/bin/unicorn_rails"
+set :unicorn_binary, "/home/vsevolod/.rvm/gems/ruby-1.9.3-p327/bin/unicorn_rails"
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{bundle_path}bundle exec script/delayed_job start"
+    run "cd #{current_path} && /usr/bin/env PATH=$PATH:/usr/local/bin RAILS_ENV=#{rails_env} script/delayed_job start"
     run "cd #{current_path} && #{bundle_path}bundle exec #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{bundle_path}bundle exec script/delayed_job stop"
+    run "cd #{current_path} && /usr/bin/env PATH=$PATH:/usr/local/bin RAILS_ENV=#{rails_env} script/delayed_job stop"
     run "kill `cat #{unicorn_pid}`"
   end
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
