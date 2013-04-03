715bb7dbb21af15ec2603c1617dfe9c786f3f6e4
Vsevolod <gsevka@gmail.com>
2013-04-04 01:05:30 +0800

    complete password change steps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Organizer::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'organizer24.ru' }
 
   # Enable threaded mode
   # config.threadsafe!
