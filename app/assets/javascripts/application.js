86b0e3e34445e1077dc73e92992dad8a7865f237
Vsevolod <gsevka@gmail.com>
2014-08-16 13:23:18 +0800

    add flot for beauty theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,9 +7,14 @@
 //= require jquery.maskedinput
 //= require jquery.jgrowl_minimized
 //= require cocoon
+//= require themes/sadmin/charts/jquery.flot
+//= require themes/sadmin/charts/jquery.flot.time
+//= require themes/sadmin/charts/jquery.flot.animator.min
+//= require themes/sadmin/charts/jquery.flot.resize.min
 //= require pages
 //= require services 
 //= require organizations
 //= require ckeditor/init
 //= require colorbox-rails
 //= require fix_ie
+//= require users
