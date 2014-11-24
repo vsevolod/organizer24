c4a17752d691ecc9e8ae8aa3c4f114cf7918586a
Vsevolod <gsevka@gmail.com>
2014-11-25 02:55:24 +0800

    fix for collection services

--- app/models/service.rb
+++ app/models/service.rb
@@ -18,7 +18,7 @@ class Service < ActiveRecord::Base
 #  validates :cost, presence: { if: Proc.new{|service| service.bottom_cost.blank? && service.top_cost.blank? }}
 
   validates :new_date_cost, presence: true, if: Proc.new{|sw| sw.new_cost.present?}
-  after_validation :check_new_cost
+  after_save :check_new_cost
 
   def check_new_cost
     update_new_cost! if self.new_cost_changed? || self.new_date_cost_changed?
@@ -26,14 +26,20 @@ class Service < ActiveRecord::Base
 
   def update_new_cost!
     Delayed::Job.where('handler ILIKE ? AND handler ILIKE ?', "%ruby/ActiveRecord:Service%", "%id: '#{self.id}'%").destroy_all
+    return self unless self.new_date_cost
     if self.new_date_cost <= Date.today
       self.cost = new_cost
       self.new_cost = nil
       self.new_date_cost = nil
+      self.save
     else
       self.delay(run_at: self.new_date_cost).update_new_cost!
       first_day = [self.new_date_cost, self.new_date_cost_was].compact.min
-      self.appointments.where(:start.gteq => first_day).each{|a| a.cost_time_by_services!(false);a.update_column(:cost, a.cost)}
+      if self.is_collection?
+        self.organization.appointments.where("? = (SELECT count(*) FROM appointments_services WHERE appointments_services.appointment_id = appointments.id AND appointments_services.service_id IN (?))", self.service_ids.count, self.service_ids)
+      else
+        self.appointments
+      end.where(:start.gteq => first_day).each{|a| a.cost_time_by_services!(false);a.update_column(:cost, a.cost)}
     end
   end
 
