3ae714b917e94e419f4b8d89e758aebaf936b5e9
Vsevolod Avramov <gsevka@gmail.com>
2016-12-01 08:57:35 +0300

    fix helper

--- app/helpers/sadmin_helper.rb
+++ app/helpers/sadmin_helper.rb
@@ -4,7 +4,7 @@ module SadminHelper
     if show_organization_page?
       "my_background_#{@organization.theme}"
     else
-      if false && current_user && current_user.owner_or_worker?(@organization)
+      if current_user && current_user.owner_or_worker?(@organization)
         'skin-blur-ocean'
       else
         "my_bg-#{@organization.theme}"
